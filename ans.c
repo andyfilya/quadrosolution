909dbc47feb94e7ed20315b7b450c04541aebe80
Andrey <liamo19@mail.ru>
2022-07-04 18:49:16 +0600

    Added function solve quadro equation (if a = 0 and a != 0)

--- ans.c
+++ ans.c
@@ -7,6 +7,46 @@ bool Equal_2(double x, double y)
 }
 bool Zero(double x)
 {
-    assert(isfinite(x));
     return (fabs(x) < EPSILON);
+}
+//a != 0
+int SolveEquation_2(double a, double b, double c, double *x_1, double *x_2)
+{
+    assert(isfinite(a) && isfinite(b) && isfinite(c));
+    assert(x_1 != NULL && x_2 != NULL);
+    if (Zero(a))
+    {
+        return SolveEquation_1;
+    }
+    double D = b * b - 4 * a * c;
+    if (D < 0)
+    {
+        return R0;
+    }
+    if (Zero(D))
+    {
+        *x_2 = (-b) / (2 * a);
+        return R1;
+    }
+    if (D > 0)
+    {
+        *x_1 = (-b + sqrt(D)) / (4 * a);
+        *x_2 = (-b - sqrt(D)) / (4 * a);
+        return R2; 
+    }
+}
+int SolveEquation_1(double b, double c, double *x_2)
+{
+    assert(isfinite(b) && isfinite(c));
+    assert(x_2 != NULL);
+    if (Zero(b))
+    {
+        if(Zero(c))
+        {
+            return R3;
+        }
+        return R0;
+    }
+    *x_2 = (-c) / b;
+    return R1;
 }
