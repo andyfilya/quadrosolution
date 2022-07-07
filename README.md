8fd573cc83b76e9ca30a0f1e32eda2c16387ae82
Andrey <liamo19@mail.ru>
2022-07-07 08:33:10 +0600

    Fixed problems

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# quadrosolution
-This program is scan the coefficients a, b, c  from equation ax2 + bx + c = 0 and give you the roots of quadro equation
+# Quadratic equation
+*This program takes three parameters as input - a, b, c - the coefficients of the quadratic equation ax^2 + bx + c = 0 and two roots are fed to the program input (or one, in the case when there is one root)*
+____
+# Future
+*In the future, we plan to add two functions to our program*
+* ***Integral equations***
+* ***Cubic equations and equations of the highest degree***
---
+++ test.cpp
@@ -0,0 +1,82 @@
+#include "head.h"
+
+
+struct param_of_quadroeq
+{
+    double a;
+    double b;
+    double c;
+    double x_1;
+    double x_2;
+    int counter_of_roots;
+};
+bool test_prog(const struct param_of_quadroeq *test_of_equation, struct param_of_quadroeq *temp_of_equation)
+{
+    assert(test_of_equation != NULL);
+    assert(temp_of_equation != NULL);
+
+    double x_1 = NAN, x_2 = NAN;
+
+    int counter_of_roots = SolveQuadratic(test_of_equation->a, test_of_equation->b, test_of_equation->c, &x_1, &x_2);
+    temp_of_equation->x_1 = x_1;
+    temp_of_equation->x_2 = x_2;
+    temp_of_equation->counter_of_roots = counter_of_roots;
+
+    switch(counter_of_roots)
+    {
+        case R2:
+            return ((Equal_2(test_of_equation->x_1, x_1) && Equal_2(test_of_equation->x_2, x_2)) || (Equal_2(test_of_equation->x_1, x_2) && (Equal_2(test_of_equation->x_2, x_1)))) && (test_of_equation->counter_of_roots == counter_of_roots);
+            break;
+        case R1:
+            return Equal_2(test_of_equation->x_1, x_1) && (test_of_equation->counter_of_roots == counter_of_roots);
+            break;
+        case R0:
+        case R3: 
+            return (test_of_equation->counter_of_roots == counter_of_roots);
+            break;
+    }
+}
+
+bool func_of_test()
+{
+    bool flag = true;
+    const struct param_of_quadroeq test_of_equation[] = 
+{
+        {0,   0,  0,   0,      0, R3},
+        {1,  -5,  6,   3,      2, R2},
+        {1,   -2, -3,   3,     -1, R2},
+        {0,  -2,  8,   4,      0, R1},
+        {1,   -7,  0,  7,      0, R2},
+        {5, 0, 30,   0, 0, R0},
+        {1,   1,  1, 0,      0, R0},
+    };
+    struct param_of_quadroeq temp_of_equation = {0};
+    int cnt = 1;
+    for (int i = 0; i < 7; i++)
+    {
+        bool res_of_program = test_prog(&test_of_equation[i], &temp_of_equation);
+        if (res_of_program)
+        {
+            printf("Test #%d is passed !!!\n", cnt);
+        }
+        temp_of_equation.x_1 = NAN;
+        temp_of_equation.x_2 = NAN;
+        temp_of_equation.counter_of_roots = NAN;
+        flag &= res_of_program;
+        cnt += 1;
+    }
+    return flag;
+}
+
+int main()
+{
+    bool res = func_of_test();
+    if(res == true)
+    {
+        printf("OK\n");
+    }
+    else
+    {
+        printf("Not OK\n");
+    }
+}
